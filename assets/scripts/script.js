e89c885491c130a4ede5b5c506e51128b6503152
Nicolas Gomollon <nicolas@gomollon.me>
2017-03-02 16:29:24 -0800

    Replaced index page with user interface prototype.

---
+++ assets/scripts/script.js
@@ -0,0 +1,205 @@
+function dataFor(studentID) {
+	if ((studentID == null) || (studentID == undefined) || (studentID.length < 8)) {
+		return JSON.parse("{}");
+	}
+	var url = "https://peterplanner.com/launch?studentID=" + studentID + "&json=1";
+	var request = new XMLHttpRequest();
+	request.open("GET", url, false);
+	request.send(null);
+	return JSON.parse(request.responseText);
+}
+
+function availableCoursesFor(student) {
+	if ((student.terms == null) || (student.terms == undefined)) {
+		return [];
+	}
+	var availableCourses = [];
+	var yearTerm = student.terms[0];
+	for (var i = 0; i < student.blocks.length; i++) {
+		var block = student.blocks[i];
+		for (var j = 0; j < block.requirements.length; j++) {
+			var req = block.requirements[j];
+			for (var k = 0; k < req.options.length; k++) {
+				var option = req.options[k];
+				var course = student.courses[option];
+				var title = course.department + " " + course.number;
+				if (course.classes != null) {
+					var classes = course.classes[yearTerm];
+					if ((classes != undefined) && clearedPrereqs(course, student)) {
+						availableCourses.push(course);
+					}
+				}
+			}
+		}
+	}
+	return availableCourses;
+}
+
+function cmpGrade(grA, grB) {
+	if ((grA.length == 0) && (grB.length == 0)) {
+		return 0;
+	} else if (grA.length == 0) {
+		return Number.NEGATIVE_INFINITY;
+	} else if (grB.length == 0) {
+		return Number.POSITIVE_INFINITY;
+	}
+	
+	var cmpGrA = grA;
+	cmpGrA = cmpGrA.replace("+", "");
+	cmpGrA = cmpGrA.replace("-", "");
+	cmpGrA = cmpGrA.toUpperCase();
+	
+	var cmpGrB = grB;
+	cmpGrB = cmpGrB.replace("+", "");
+	cmpGrB = cmpGrB.replace("-", "");
+	cmpGrB = cmpGrB.toUpperCase();
+	
+	var comparison = 0;
+	if (cmpGrA == cmpGrB) {
+		var grAplus = grA.includes("+");
+		var grAminus = grA.includes("-");
+		
+		var grBplus = grB.includes("+");
+		var grBminus = grB.includes("-");
+		
+		if (grAplus && !grBplus) {
+			comparison = -1;
+		} else if (!grAplus && grBplus) {
+			comparison = 1;
+		} else if (!grAminus && grBminus) {
+			comparison = -1;
+		} else if (grAminus && !grBminus) {
+			comparison = 1;
+		}
+	} else if (cmpGrA < cmpGrB) {
+		comparison = -1;
+	} else if (cmpGrA > cmpGrB) {
+		comparison = 1;
+	}
+	
+	return comparison;
+}
+
+function clearedPrereqs(course, student) {
+	var numPrereqs = course.prerequisites;
+	if (numPrereqs == null) {
+		numPrereqs = 0;
+	} else {
+		numPrereqs = numPrereqs.length;
+	}
+	for (var i = 0; i < numPrereqs; i++) {
+		var prereqsAND = course.prerequisites[i];
+		var satisfied = false;
+		for (var j = 0; j < prereqsAND.length; j++) {
+			var prereqOR = prereqsAND[j];
+			if (!satisfied) {
+				var splitPrrq = prereqOR.split("|");
+				var prereq = splitPrrq[0].replace(/\s/g , "");
+				satisfied = (student.taken[prereq] !== undefined);
+				if (!satisfied && splitPrrq[0].startsWith("NO ")) {
+					prereq = prereq.slice(2);
+					satisfied = !student.taken[prereq];
+				} else if (satisfied && (splitPrrq.length == 2)) {
+					var c = student.courses[prereq];
+					var grade = splitPrrq[1];
+					if ((c.grade.length != 0) && (grade.length != 0)) {
+						if (cmpGrade(c.grade, grade) > 0) {
+							satisfied = false;
+						}
+					}
+				}
+				if (satisfied) {
+					break;
+				}
+			}
+		}
+		if (!satisfied) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function weekdays(day) {
+	switch (day) {
+	case 0:
+		return "Su";
+	case 1:
+		return "M";
+	case 2:
+		return "Tu";
+	case 3:
+		return "W";
+	case 4:
+		return "Th";
+	case 5:
+		return "F";
+	case 6:
+		return "Sa";
+	default:
+		return "";
+	}
+}
+
+function twelveHours(h) {
+	var hours = h;
+	if (hours > 12) {
+		hours -= 12;
+	}
+	return hours;
+}
+
+function meridiem(h) {
+	return (h >= 12) ? "p" : "";
+}
+
+function zeroPad(m) {
+	return ("00" + m).substr(-2);
+}
+
+function printCourses(courses) {
+	var coursesHTML = "";
+	for (var i = 0; i < courses.length; i++) {
+		var course = courses[i];
+		coursesHTML += '<div class="course">\
+		<h1>'+ course.department + ' ' + course.number + ': ' + course.title + '</h1>';
+		var description = course.description;
+		if (description != undefined) {
+			coursesHTML += '<p>' + course.description + '</p>';
+		}
+		coursesHTML += '<div class="title">\
+			<div class="code">Code</div>\
+			<div class="type">Type</div>\
+			<div class="sec">Sec</div>\
+			<div class="instructor">Instructor</div>\
+			<div class="days">Days</div>\
+			<div class="time">Time</div>\
+			<div class="place">Place</div>\
+		</div>';
+		for (var j = 0; j < course.classes["2017-14"].length; j++) {
+			var c = course.classes["2017-14"][j];
+			var timeStart = new Date(c.time.start);
+			var timeEnd = new Date(c.time.end);
+			coursesHTML += '<a class="class" href="#">\
+			<div class="code">' + c.code + '</div>\
+			<div class="type">' + c.type + '</div>\
+			<div class="sec">' + c.section + '</div>\
+			<div class="instructor">' + c.instructor + '</div>\
+			<div class="days">' + c.days.map(weekdays).join("") + '</div>\
+			<div class="time">\
+				<span class="start">' + twelveHours(timeStart.getUTCHours()) + ':' + zeroPad(timeStart.getUTCMinutes()) + '</span><span class="meridiem">' + meridiem(timeStart.getUTCHours()) + '</span><span class="sep">-</span><span class="end">' + twelveHours(timeEnd.getUTCHours()) + ':' + zeroPad(timeEnd.getUTCMinutes()) + '</span><span class="meridiem">' + meridiem(timeEnd.getUTCHours()) + '</span>\
+			</div>\
+			<div class="place">' + c.place + '</div>\
+		</a>';
+		}
+		coursesHTML += '</div>';
+		if (i < (courses.length - 1)) {
+			coursesHTML += '<div class="separator"></div>';
+		}
+	}
+	document.getElementById("courses").innerHTML = coursesHTML;
+}
+
+var student = dataFor(studentID);
+var courses = availableCoursesFor(student);
+printCourses(courses);
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Peter Planner</title>
+	<meta name="description" content="Peter Planner is a prerequisite tracker and scheduling tool for UCI students." />
+	<meta name="robots" content="index, follow" />
+	<link rel="stylesheet" type="text/css" href="assets/stylesheets/style.css">
+	<!--[if IE]><script type="text/javascript" src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+	<script type="text/javascript">
+		var studentID = <?php echo isset($_GET['studentID']) ? "\"".$_GET['studentID']."\"" : "null" ?>;
+	</script>
+</head>
+
+<body>
+	<div id="courses" class="courses"></div>
+	<script type="text/javascript" src="assets/scripts/script.js"></script>
+</body>
+</html>
